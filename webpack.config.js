b265debb138cf8b61d2e5728f03bdfe21453a210
Kuni <121762219+kuneus@users.noreply.github.com>
2023-05-23 00:09:16 -0400

    Add starter files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'IdeaList',
+      header: 'IdeaList',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  mode: 'development',
+  output: {
+    clean: true,
+  },
+};
